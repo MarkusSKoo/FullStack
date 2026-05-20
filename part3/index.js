7424584ddff9f8f4a5edd039b3d0beb042f318ca
MarkusSKoo <markus.kauhanen@helsinki.fi>
2026-05-20 15:38:45 +0300

    uibuild

--- part3/index.js
+++ part3/index.js
@@ -53,7 +53,7 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.some(person => person.name === body.name)) {
+    if (person.some(person => person.name === body.name)) {
         return response.status(400).json({
             error: 'name must be unique'
         })
