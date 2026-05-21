b8981394ae14250ae8dcb8c8d26292b75c9cfbdd
MarkusSKoo <markus.kauhanen@helsinki.fi>
2026-05-21 11:42:36 +0300

    uibuild

--- part3/models/person.js
+++ part3/models/person.js
@@ -23,7 +23,7 @@ const personSchema = new mongoose.Schema({
         type: String,
         validate: {
             validator: function (v) {
-                return /^\d{2,3}-{1}\d{5,}$/.test(v)
+                return /^\d{2,3}-\d{5,}$/.test(v)
             },
             message: props => `${props.value} is not a valid phone number`
         },
