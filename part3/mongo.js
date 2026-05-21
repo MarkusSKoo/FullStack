5af336ddfb70e68a953838d227675c9a97c30976
MarkusSKoo <markus.kauhanen@helsinki.fi>
2026-05-21 12:25:30 +0300

    uibuild

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,8 +13,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -23,28 +23,28 @@ const number = process.argv[4]
 
 if (process.argv.length === 5) {
 
-    const person = new Person({
-        name: name,
-        number: number,
-    })
+  const person = new Person({
+    name: name,
+    number: number,
+  })
 
-    person
-        .save()
-        .then(result => {
-            console.log('person saved!')
-            mongoose.connection.close()
-        })
+  person
+    .save()
+    .then(() => {
+      console.log('person saved!')
+      mongoose.connection.close()
+    })
 
 }
 
 if (process.argv.length === 3) {
 
-    Person
-        .find({})
-        .then(persons => {
-            persons.forEach(person => {
-                console.log(person)
-                mongoose.connection.close()
-            })
-        })
+  Person
+    .find({})
+    .then(persons => {
+      persons.forEach(person => {
+        console.log(person)
+        mongoose.connection.close()
+      })
+    })
 }
